0e5078d308b1b886e21d7d46a2b6287c18377196
DESKTOP-V1IS21O\Puesto8 <frandanigd@gmail.com>
2023-10-09 14:42:57 +0200

    09/10/2023

---
+++ angular-basico2/proyecto001/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'proyecto001';
+  autor = "Francisco Jose Gallego Dorado";
+  color = "blue";
+  tituloPagina = "Mi primera App con Angular";
+}
