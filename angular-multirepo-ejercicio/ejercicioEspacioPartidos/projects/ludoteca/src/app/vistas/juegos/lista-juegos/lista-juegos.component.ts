c16ff4240f7391ff32696643680ebb75baf64180
FRAN\fran <frandanigd@gmail.com>
2023-11-26 20:26:30 +0100

    26/11/2023 ludoteca terminada

--- angular-multirepo-ejercicio/ejercicioEspacioPartidos/projects/ludoteca/src/app/vistas/juegos/lista-juegos/lista-juegos.component.ts
+++ angular-multirepo-ejercicio/ejercicioEspacioPartidos/projects/ludoteca/src/app/vistas/juegos/lista-juegos/lista-juegos.component.ts
@@ -1,23 +1,38 @@
 import { Component } from '@angular/core';
 import { FirebaseService } from '../../../servicios/firebase.service';
 import { Juego } from '../../../modelos/juego';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-lista-juegos',
   templateUrl: './lista-juegos.component.html',
-  styleUrls: ['./lista-juegos.component.css']
+  styleUrls: ['./lista-juegos.component.css'],
 })
 export class ListaJuegosComponent {
-
   juegos?: Juego[];
   constructor(private fbs: FirebaseService) {}
 
   ngOnInit(): void {
-    this.fbs.getFireBase("juegos").subscribe(res => this.juegos = res);
+    this.fbs.getFireBase('juegos').subscribe((res) => (this.juegos = res));
   }
 
   // Método que elimina un juego en la base de datos
-  deleteJuego(juego: Juego){
-    this.fbs.deleteFireBase(juego, "juegos");
+  deleteJuego(juego: Juego) {
+    this.fbs
+      .deleteFireBase(juego, 'juegos')
+      .then(() =>
+        Swal.fire({
+          title: 'Eliminado!',
+          text: 'Juego ha sido eliminado',
+          icon: 'success',
+        })
+      )
+      .catch(() =>
+        Swal.fire({
+          title: 'Oops...!',
+          text: 'El juego no ha sido eliminado',
+          icon: 'error',
+        })
+      );
   }
 }
--- angular-multirepo-ejercicio/ejercicioEspacioPartidos/projects/ludoteca/src/app/vistas/usuarios/detalle-usuario/detalle-usuario.component.ts
+++ angular-multirepo-ejercicio/ejercicioEspacioPartidos/projects/ludoteca/src/app/vistas/usuarios/detalle-usuario/detalle-usuario.component.ts
@@ -1,32 +1,48 @@
 import { Component, Input } from '@angular/core';
 import { Usuario } from '../../../modelos/usuario';
 import { FirebaseService } from '../../../servicios/firebase.service';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-detalle-usuario',
   templateUrl: './detalle-usuario.component.html',
-  styleUrls: ['./detalle-usuario.component.css']
+  styleUrls: ['./detalle-usuario.component.css'],
 })
 export class DetalleUsuarioComponent {
-  
-  id: string = "";
+  id: string = '';
   usuario?: Usuario;
   constructor(private fbs: FirebaseService, private route: ActivatedRoute) {}
 
-  ngOnInit(){
-    this.id = this.route.snapshot.paramMap.get("id")!;
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id')!;
     this.devuelveUsuarioId();
   }
 
   // Método que busca el usuario segun el id
   devuelveUsuarioId(): void {
-    this.fbs.getFireBasePorId("usuarios", this.id)
-            .subscribe(res => this.usuario = res);
+    this.fbs
+      .getFireBasePorId('usuarios', this.id)
+      .subscribe((res) => (this.usuario = res));
   }
 
   // Método que actualiza el usuario en la bbdd
   actualizaUsuario(): void {
-    this.fbs.updateFireBase(this.usuario, "usuarios", this.usuario?.id!);
+    this.fbs
+      .updateFireBase(this.usuario, 'usuarios', this.usuario?.id!)
+      .then(() =>
+        Swal.fire({
+          title: 'Actualizado!',
+          text: 'Usuario ha sido actualizado',
+          icon: 'success',
+        })
+      )
+      .catch(() =>
+        Swal.fire({
+          title: 'Oops...!',
+          text: 'El usuario no ha sido actualizado',
+          icon: 'error',
+        })
+      );
   }
 }
